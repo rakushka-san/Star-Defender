47bb04018dca1fb553e948b2ac38cd92b84e6248
rakushka-san <vladislavmohov@mail.ru>
2022-03-20 14:34:46 +0300

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+import random
 import os
 
 
@@ -19,6 +20,11 @@ game_folder = os.path.dirname(__file__)
 print(game_folder)
 img_folder = os.path.join(game_folder, 'img')
 player_img = pg.image.load(os.path.join(img_folder, 'starship.png')).convert()
+meteor_img = pg.image.load(os.path.join(img_folder, 'meteor.png')).convert()
+bullet_img = pg.image.load(os.path.join(img_folder, 'bullet.png')).convert()
+bg_img = pg.image.load(os.path.join(img_folder, 'background.png')).convert()
+bg_rect = bg_img.get_rect()
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self):
@@ -64,20 +70,81 @@ class Player(pg.sprite.Sprite):
         if self.rect.top < 0:
             self.rect.top = 0
 
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
+
+
+class Mob(pg.sprite.Sprite):
+    def __init__(self):
+        pg.sprite.Sprite.__init__(self)
+        self.image = meteor_img
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(1, 10)
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -40)
+            self.speedy = random.randrange(4, 10)
+
+class Bullet(pg.sprite.Sprite):
+    def __init__(self, x, y):
+        pg.sprite.Sprite.__init__(self)
+        self.image = bullet_img
+        self.image.set_colorkey(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
 all_sprites = pg.sprite.Group()
 player = Player()
 all_sprites.add(player)
+
+mobs = pg.sprite.Group()
+bullets = pg.sprite.Group()
+
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
 
 running = True
 while running:
     clock.tick(FPS)
     all_sprites.update()
 
+    hits_player = pg.sprite.spritecollide(player, mobs, False)
+    if hits_player:
+        running = False
+
+    hits_mobs = pg.sprite.groupcollide(bullets, mobs, True, True)
+    for hit in hits_mobs:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+        elif event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                player.shoot()
+
 
     screen.fill(BLACK)
+    screen.blit(bg_img, bg_rect)
     all_sprites.draw(screen)
     pg.display.flip()
 
